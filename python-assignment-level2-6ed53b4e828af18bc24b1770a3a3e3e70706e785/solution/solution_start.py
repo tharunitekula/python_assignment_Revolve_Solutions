190f7bf008a688ea000ac0b8372b5b427506ee62
tharunitekula <75466828+tharunitekula@users.noreply.github.com>
2023-05-25 14:37:20 +0530

    Add files via upload

---
+++ python-assignment-level2-6ed53b4e828af18bc24b1770a3a3e3e70706e785/solution/solution_start.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+
+def get_params() -> dict:
+    parser = argparse.ArgumentParser(description='DataTest')
+    parser.add_argument('--customers_location', required=False, default="./input_data/starter/customers.csv")
+    parser.add_argument('--products_location', required=False, default="./input_data/starter/products.csv")
+    parser.add_argument('--transactions_location', required=False, default="./input_data/starter/transactions/")
+    parser.add_argument('--output_location', required=False, default="./output_data/outputs/")
+    return vars(parser.parse_args())
+
+
+
+def main():
+    params = get_params()
+    
+
+if __name__ == "__main__":
+    main()
